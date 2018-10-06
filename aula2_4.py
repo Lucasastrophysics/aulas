6f1b3c20bd463a9143c19d4315cd13dc541d6189
Lucasastrophysics <43917394+Lucasastrophysics@users.noreply.github.com>
2018-10-06 19:16:35 -0300

    Add files via upload

---
+++ aula2_4.py
@@ -0,0 +1,7 @@
+#aula2_4
+comprimento_de_onda=632.8*10**(-9) 
+D=1.98
+d=0.250*10**(-3)
+Delta_lambda=((comprimento_de_onda*D)/d)*10**(3) 
+
+print('O valor da distância entre os máximos é de ',Delta_lambda,'milímetros')
