cb6d244cdba8428a8edece3232daca994772707a
Lucasastrophysics <43917394+Lucasastrophysics@users.noreply.github.com>
2018-10-06 18:40:37 -0300

    Add files via upload

---
+++ aula2_1.py
@@ -0,0 +1,16 @@
+#aula2_exercicio1
+largura=17
+altura=12.0
+delimitador="."
+
+s=largura/2
+z=largura/2.0
+x=1+2*5
+y=delimitador*5
+
+print('o valor de largura/2 é',s)
+print('o valor de largura/2.0 é',z)
+print('o valor de 1+2*5 é',x)
+print('o rosultado de delimitador*5 é',z)
+
+#End_program
