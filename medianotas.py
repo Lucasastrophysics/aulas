e9956e95bdeac6e3a9667bcb61ae3f527beb83ea
Lucasastrophysics <43917394+Lucasastrophysics@users.noreply.github.com>
2018-10-29 11:11:48 -0300

    Add files via upload

---
+++ medianotas.py
@@ -0,0 +1,8 @@
+#Programa média
+def media():
+	x=float(input('insira a primeira nota'))	
+	y=float(input('insira a sugunda nota'))
+	m=(x+y)/2
+	print('O valor da média é: {:.1f}'.format(m))
+media()
+
